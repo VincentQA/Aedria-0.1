18502beb7a74e1d1661a8287204e20cd3c37cd78
VincentQA <144799030+VincentQA@users.noreply.github.com>
2024-09-22 22:12:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,23 @@ st.set_page_config(page_title="AedrIA", page_icon="📚")
 
 # Titre et image de la page d'accueil
 st.title("AedrIA")
-st.image("images/accueil_image.jpg", use_column_width=True)  # Remplacez par le nom de votre image d'accueil
+# Si vous souhaitez retirer l'image d'accueil, commentez ou supprimez la ligne suivante
+# st.image("images/accueil_image.jpg", use_column_width=True)
 st.write("Bienvenue dans notre application de lecture interactive !")
 
-# Liste des histoires avec leurs images et noms de fichiers
+# Liste des histoires avec leurs émojis et noms de fichiers
 histoires = [
     {
         "titre": "Rencontre sur le court",
-        "image": "images/couverture_rencontre.jpg",
+        "emoji": "🎾",
         "page": "Rencontre sur le court"
     },
-   {
+    {
         "titre": "Sans état d'âme",
-        "image": "images/couverture_rencontre.jpg",
+        "emoji": "🕵️‍♂️",
         "page": "Sans-etat-ame"
     },
     # Ajoutez d'autres histoires ici
-
 ]
 
 # Affichage des vignettes sous forme de grille
@@ -29,9 +29,12 @@ cols = st.columns(3)
 for idx, histoire in enumerate(histoires):
     col = cols[idx % 3]
     with col:
-        st.image(histoire["image"], use_column_width=True)
+        # Afficher l'émoji à la place de l'image
+        st.markdown(
+            f"<div style='font-size:60px; text-align:center;'>{histoire['emoji']}</div>",
+            unsafe_allow_html=True
+        )
         # Création d'un lien vers la page de l'histoire
         page_name = histoire["page"].replace(' ', '_')
         link = f"[**{histoire['titre']}**]({os.path.join('pages', page_name + '.py')})"
         st.markdown(link, unsafe_allow_html=True)
-    
