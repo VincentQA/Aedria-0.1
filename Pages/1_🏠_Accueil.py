bafc248a9663b956ee160fd5464eab1f096edf77
VincentQA <144799030+VincentQA@users.noreply.github.com>
2024-09-22 22:40:14 +0200

    Delete Pages directory

--- Pages/1_🏠_Accueil.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="AedrIA", page_icon="📚")
-
-# Titre et image d'accueil (si vous le souhaitez)
-st.title("AedrIA")
-st.write("Bienvenue dans notre application de lecture interactive !")
-
-st.sidebar.success("Sélectionnez une histoire dans le menu ci-dessus.")
