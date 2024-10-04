82c81a0c8941ab9fd941880f1398887a19b14d2b
VincentQA <144799030+VincentQA@users.noreply.github.com>
2024-10-04 16:32:34 +0200

    Update 3_🐺_legende_loup_blanc.py

--- pages/3_🐺_legende_loup_blanc.py
+++ pages/3_🐺_legende_loup_blanc.py
@@ -25,7 +25,7 @@ if "checkpoint" not in st.session_state:
     st.session_state.checkpoint = 1  # Suivi du checkpoint actuel
 
 # Titre de l'application
-st.title(" La Légende du   ")
+st.title(" La Légende du loup blanc ")
 st.subheader("Une aventure interactive où vos choix façonnent l'histoire")
 
 # Fonction pour créer un nouveau thread pour un assistant s'il n'existe pas encore
