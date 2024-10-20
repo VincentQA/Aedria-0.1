6e17bb8c98548f2ea1965e4b841f21b1f9de3ac2
VincentQA <144799030+VincentQA@users.noreply.github.com>
2024-10-20 09:59:17 +0200

    Update 2_👨‍💻_Nouvel_Assistant.py

--- pages/2_👨‍💻_Nouvel_Assistant.py
+++ pages/2_👨‍💻_Nouvel_Assistant.py
@@ -28,9 +28,8 @@ if "checkpoint" not in st.session_state:
 st.title("👨‍💻 Le nouvel assistant")
 st.subheader("Une aventure interactive où vos choix façonnent l'histoire")
 st.markdown("Votre avis est précieux ! Partagez vos impressions et aidez-nous à améliorer l'application en répondant à notre questionnaire rapide.")
-if st.button('Répondre au questionnaire', key='questionnaire_button', help='Cliquez pour accéder au questionnaire'):
-    js = "window.open('https://forms.gle/4FiNG71ZP7bWFcV4A')"
-    st.components.v1.html(f"<script>{js}</script>", height=0)
+if st.button('Répondre au questionnaire'):
+    st.write('[Cliquez ici pour accéder au questionnaire](https://forms.gle/4FiNG71ZP7bWFcV4A)')
 
 # Fonction pour créer un nouveau thread pour un assistant s'il n'existe pas encore
 def initialize_thread(assistant_role):
